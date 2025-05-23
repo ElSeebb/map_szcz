14d646d075a970108393a6f475b0a21b8867d898
Sebastian <sebcie95@wp.pl>
2025-05-23 14:11:44 +0200

    marker generator funkcja osobna

--- date/marker.js
+++ date/marker.js
@@ -899,21 +899,21 @@ export const places = [
 
 // Funkcja do tworzenia markera i obsługi kliknięcia
 
-places.forEach((place) => {
-	const marker = L.marker(place.coords).addTo(map);
-	marker.on('click', () => {
-		const panel = document.getElementById('panel');
-		panel.innerHTML = `
-        <img src="${place.img}" alt="Tablica ${place.title}" style="width: 100%; height: auto; border-radius: 8px;">
-        <h2>${place.title}</h2> 
-        <p>${place.address}</p>
-        <p>${place.description}</p>
-        <button onclick="zamknijPanel()">Zamknij</button>
-      `;
-		panel.classList.add('aktywny');
-	});
-});
+// places.forEach((place) => {
+// 	const marker = L.marker(place.coords).addTo(map);
+// 	marker.on('click', () => {
+// 		const panel = document.getElementById('panel');
+// 		panel.innerHTML = `
+//         <img src="${place.img}" alt="Tablica ${place.title}" style="width: 100%; height: auto; border-radius: 8px;">
+//         <h2>${place.title}</h2> 
+//         <p>${place.address}</p>
+//         <p>${place.description}</p>
+//         <button onclick="zamknijPanel()">Zamknij</button>
+//       `;
+// 		panel.classList.add('aktywny');
+// 	});
+// });
 
-function zamknijPanel() {
-	document.getElementById('panel').classList.remove('aktywny');
-}
+// function zamknijPanel() {
+// 	document.getElementById('panel').classList.remove('aktywny');
+// }
---
+++ js/markerGen.js
@@ -0,0 +1,21 @@
+import {places} from '../date/marker.js'
+
+
+places.forEach((place) => {
+    const marker = L.marker(place.coords).addTo(map);
+    marker.on('click', () => {
+        const panel = document.getElementById('panel');
+        panel.innerHTML = `
+        <img src="${place.img}" alt="Tablica ${place.title}" style="width: 100%; height: auto; border-radius: 8px;">
+        <h2>${place.title}</h2> 
+        <p>${place.address}</p>
+        <p>${place.description}</p>
+        <button onclick="zamknijPanel()">Zamknij</button>
+      `;
+        panel.classList.add('aktywny');
+    });
+});
+
+function zamknijPanel() {
+    document.getElementById('panel').classList.remove('aktywny');
+}
