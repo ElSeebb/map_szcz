fe9c2885efb7f8a919d58bd083c3026cd5d8533e
Sebastian <sebcie95@wp.pl>
2025-05-25 12:01:40 +0200

    przełączanie między markerami anim

--- js/markerGen.js
+++ js/markerGen.js
@@ -1,26 +1,5 @@
-import {places} from '../date/marker.js'
-import {map} from '../js/map.js'
-
-// places.forEach((place) => {
-//     const marker = L.marker(place.coords).addTo(map);
-//     marker.on('click', () => {
-//         const panel = document.getElementById('panel');
-//         panel.innerHTML = `
-//         <img src="${place.img}" alt="Tablica ${place.title}" style="width: 100%; height: auto; border-radius: 8px;">
-//         <h2>${place.title}</h2> 
-//         <p>${place.address}</p>
-//         <p>${place.description}</p>
-//         <button onclick="zamknijPanel()">Zamknij</button>
-//       `;
-//         panel.classList.add('aktywny');
-//         document.getElementById('zamknij-btn').addEventListener('click', () => {
-// 		panel.classList.remove('aktywny');
-//     });
-// });
-
-// function zamknijPanel() {
-//     document.getElementById('panel').classList.remove('aktywny');
-// }
+import { places } from '../date/marker.js';
+import { map } from '../js/map.js';
 
 places.forEach((place) => {
 	const marker = L.marker(place.coords).addTo(map);
@@ -28,26 +7,40 @@ places.forEach((place) => {
 	marker.on('click', () => {
 		const panel = document.getElementById('panel');
 
-		panel.innerHTML = `
-			<img src="${place.img}" alt="Tablica ${place.title}" style="width: 100%; height: auto; border-radius: 8px;">
-			<h2>${place.title}</h2> 
-			<p>${place.address}</p>
-			<p>${place.description}</p>
-			<button id="zamknij-btn">Zamknij</button>
-		`;
-
-		panel.classList.add('aktywny');
-
-		document.getElementById('zamknij-btn').addEventListener('click', () => {
+		if (panel.classList.contains('aktywny')) {
 			panel.classList.remove('aktywny');
-		});
+
+			setTimeout(() => {
+				loadPanelDesc(panel, place);
+			}, 200);
+		} else {
+			loadPanelDesc(panel, place);
+		}
 	});
 });
+
+function loadPanelDesc(panel, place) {
+	panel.innerHTML = `
+		<img src="${place.img}" alt="Tablica ${place.title}" style="width: 100%; height: auto; border-radius: 8px;">
+		<h2>${place.title}</h2> 
+		<p>${place.address}</p>
+		<p>${place.description}</p>
+		<button id="zamknij-btn">Zamknij</button>
+	`;
+
+	panel.classList.add('aktywny');
+
+	document.getElementById('zamknij-btn').addEventListener('click', () => {
+		panel.classList.remove('aktywny');
+	});
+}
 
 document.addEventListener('click', (e) => {
 	const panel = document.getElementById('panel');
 
-	const isMarker = e.target.closest('.leaflet-marker-icon, .leaflet-interactive');
+	const isMarker = e.target.closest(
+		'.leaflet-marker-icon, .leaflet-interactive'
+	);
 	const isPanel = panel.contains(e.target);
 
 	if (panel.classList.contains('aktywny') && !isMarker && !isPanel) {
